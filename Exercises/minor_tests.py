8c86b12e5a58c1b54771459b12e74a008fff7b26
Alexey <alllexey1997@gmail.com>
2021-07-13 00:42:36 +0300

    123

--- Exercises/minor_tests.py
+++
@@ -1,8 +0,0 @@
-x = ['i', 't', ' ', ' ', 'w', 'a', 's', ' ', ' ', 'a', ' ', ' ', 'g', 'o', 'o', 'd', ' ', ' ', 'd', 'a', 'y']
-idx = 0
-while idx < len(x):
-    if x[idx] == ' ':
-        x.pop(idx)
-    else:
-        idx += 1
-print(x)
