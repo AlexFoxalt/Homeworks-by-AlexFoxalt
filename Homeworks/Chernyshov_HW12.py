8797a9dfce8a040506c41976a87b527aa9dac803
Alexey <alllexey1997@gmail.com>
2021-07-30 23:31:29 +0300

    some changes

--- Homeworks/Chernyshov_HW12.py
+++ Homeworks/Chernyshov_HW12.py
@@ -6,7 +6,7 @@ from math import sqrt
  который бы возвращал площадь треугольника.
  
  *Реализуйте в классе Triangle возможность итерировать его обьекты по вершинам (точкам)
- """
+"""
 
 
 class Point:
@@ -27,6 +27,7 @@ class Triangle:
                 self.a = a
                 self.b = b
                 self.c = c
+                # По формуле Хряпы))
                 self.a_b_length = (((b.x_coord - a.x_coord) ** 2) + ((b.y_coord - a.y_coord) ** 2)) ** 0.5
                 self.b_c_length = (((c.x_coord - b.x_coord) ** 2) + ((c.y_coord - b.y_coord) ** 2)) ** 0.5
                 self.c_a_length = (((a.x_coord - c.x_coord) ** 2) + ((a.y_coord - c.y_coord) ** 2)) ** 0.5
@@ -36,7 +37,7 @@ class Triangle:
             raise Exception('Incorrect type of data')
 
     def area_of_triangle(self):
-
+        """По формуле Герона"""
         p = (self.a_b_length + self.b_c_length + self.c_a_length) * 0.5
         area = sqrt(p * (p - self.a_b_length) * (p - self.b_c_length) * (p - self.c_a_length))
         print(round(area, 2))
@@ -57,7 +58,7 @@ class Triangle:
         return res
 
 
-figure1 = Triangle(Point(0, 0), Point(5, 5), Point(0, 5))
+figure1 = Triangle(Point(0, 1), Point(2, 5), Point(0, 5))
 # figure2 = Triangle(Point('0', '0'), Point(False, True), Point(0, 0))
 # figure3 = Triangle(Point(0, 0), Point(0, 0), Point(0, 0))
 
