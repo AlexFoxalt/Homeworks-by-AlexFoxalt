1a3d3f20c0356f9ff411b4fdfd5d91fc52f48863
Alexey <alllexey1997@gmail.com>
2021-07-31 11:28:10 +0300

    some changes

--- Homeworks/Chernyshov_HW12.py
+++ Homeworks/Chernyshov_HW12.py
@@ -47,7 +47,7 @@ class Triangle:
         # По формуле Герона
         p = (self.a_b_length + self.b_c_length + self.c_a_length) * 0.5
         area = sqrt(p * (p - self.a_b_length) * (p - self.b_c_length) * (p - self.c_a_length))
-        print(round(area, 2))
+        print('Area =', round(area, 2))
         return
 
     def __iter__(self):
